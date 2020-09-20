a4f45a1eb9fe0fc5b1305540fa65bc18049f6f91
kabu1 <kkabubenson@gmail.com>
2020-09-20 12:37:04 -0400

    initiate migration

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
