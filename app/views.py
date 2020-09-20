7d50593777a0335a1632a8bb3820f1a3bf4d69df
kabu1 <kkabubenson@gmail.com>
2020-09-20 06:58:17 -0400

    add configuration

--- app/views.py
+++ app/views.py
@@ -9,22 +9,3 @@ def index():
     '''
     title = 'Home - Welcome to the Pitch Website'
     return render_template('index.html',title=title)
-@app.route('/categories')
-
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    general = Pitch.query.filter_by(category="general").order_by(Pitch.posted.desc()).all()
-    project = Pitch.query.filter_by(category="project").order_by(Pitch.posted.desc()).all()
-    advertisement = Pitch.query.filter_by(category="advertisement").order_by(Pitch.posted.desc()).all()
-    sale = Pitch.query.filter_by(category="sale").order_by(Pitch.posted.desc()).all()
-
-    pitch = Pitch.query.filter_by().first()
-    likes = Like.get_all_likes(pitch_id=Pitch.id)
-    dislikes = Dislike.get_all_dislikes(pitch_id=Pitch.id)
-
-
-    title = 'Home | One Min Pitch'
-    return render_template('index.html', title = title, pitch = pitch, general = general, project = project, advertisement = advertisement, sale = sale, likes=likes, dislikes=dislikes)
