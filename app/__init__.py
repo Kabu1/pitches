c6d8d425ff35ba4600558ea3c7f3748d072b5bab
kabu1 <kkabubenson@gmail.com>
2020-09-21 02:05:13 -0400

    add flask login

--- app/__init__.py
+++ app/__init__.py
@@ -14,8 +14,16 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-
+    db.init_app(app)
+    login_manager.init_app(app)
 
     # Setting up configuration
     app.config.from_object(DevConfig)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
     return app
+
+    
+    
