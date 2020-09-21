0b4160d4b56f8c46ee872ed59b39d41718fff841
kabu1 <kkabubenson@gmail.com>
2020-09-21 06:48:01 -0400

    add login manager

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,32 @@
 from flask import render_template
 from app import app
+from flask_login import login_required
 
 #views
-@app.route('/')
+@main.route('/')
 def index():
     '''
     view root page function that returns the index page and its date
     '''
     title = 'Home - Welcome to the Pitch Website'
     return render_template('index.html',title=title)
+
+@main.route('/',methods = ['GET', 'POST'])
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    general = Pitch.query.filter_by(category="general").order_by(Pitch.posted.desc()).all()
+    project = Pitch.query.filter_by(category="project").order_by(Pitch.posted.desc()).all()
+    advertisement = Pitch.query.filter_by(category="advertisement").order_by(Pitch.posted.desc()).all()
+    sale = Pitch.query.filter_by(category="sale").order_by(Pitch.posted.desc()).all()
+
+    pitch = Pitch.query.filter_by().first()
+    likes = Like.get_all_likes(pitch_id=Pitch.id)
+    dislikes = Dislike.get_all_dislikes(pitch_id=Pitch.id)
+
+
+    title = 'Home | One Min Pitch'
+    return render_template('index.html', title = title, pitch = pitch, general = general, project = project, advertisement = advertisement, sale = sale, likes=likes, dislikes=dislikes)
--- app/models.py
+++ app/models.py
@@ -1,25 +1,33 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
-    __tablename__ = 'users'
+class User(UserMixin,db.Model):
+    __tablename__='users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
-        @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+    likes = db.relationship('Like',backref = 'user',lazy = "dynamic")
+    dislikes = db.relationship('Dislike',backref = 'user',lazy = "dynamic")
 
+    @property
+    def password(self):
+        raise AttributeError('You Cannot read the password attribute')
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
