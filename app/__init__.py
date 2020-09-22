6cfa771c1b93a88e6a5fc905964604434565713d
kabu1 <kkabubenson@gmail.com>
2020-09-22 02:31:54 -0400

    fix bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import DevConfig
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -14,21 +14,23 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
-    # 
+
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    db.init_app(app)
     login_manager.init_app(app)
 
-    # Setting up configuration
-    app.config.from_object(DevConfig)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
-
-    return app
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     
+    return app
     
